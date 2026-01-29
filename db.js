0b1c1a082b29e1b41f09f4feba942f15d2564c11
Rodrigo Dominguez <licrodrigo3x3@gmail.com>
2026-01-28 20:17:14 -0600

    Fix: export correcto de conectarDB para Render

--- db.js
+++ db.js
@@ -3,12 +3,29 @@ const sql = require('mssql');
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
+    server: process.env.DB_SERVER, // sql.somee.com
     database: process.env.DB_NAME,
     options: {
         encrypt: true,
         trustServerCertificate: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
     }
 };
 
-module.exports = sql.connect(config);
+let pool = null;
+
+const conectarDB = async () => {
+    if (!pool) {
+        pool = await sql.connect(config);
+    }
+    return pool;
+};
+
+module.exports = {
+    sql,
+    conectarDB
+};
