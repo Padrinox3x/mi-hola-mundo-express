edbf72e5af55f552e6be33a4843e7ef9ce814ed6
Rodrigo Dominguez <licrodrigo3x3@gmail.com>
2026-02-25 20:24:11 -0600

    correcion del bd

--- db.js
+++ db.js
@@ -3,21 +3,32 @@ const sql = require('mssql');
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER, // EjemploExpress.mssql.somee.com
+    server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
     options: {
-        encrypt: true,
+        encrypt: false,                 // 🔴 CLAVE
         trustServerCertificate: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
     }
 };
 
 let pool;
 
 async function conectarDB() {
-    if (!pool) {
-        pool = await sql.connect(config);
+    try {
+        if (!pool) {
+            pool = await new sql.ConnectionPool(config).connect();
+            console.log('✅ Pool SQL creado');
+        }
+        return pool;
+    } catch (err) {
+        console.error('❌ Error al conectar DB:', err);
+        throw err;
     }
-    return pool;
 }
 
 module.exports = {
